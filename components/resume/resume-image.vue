e158501eaf1f2f953507b305d0adf52f2b48e6d1
Hay Zohar <hay12396@gmail.com>
2021-12-21 21:36:05 +0200

    test2

--- components/resume/resume-image.vue
+++ components/resume/resume-image.vue
@@ -90,7 +90,6 @@ export default {
     .image {
       position: relative;
       margin-left: 1rem;
-      min-height: 300px;
       
       @media screen and (min-width: 768px) {
         margin-left: 0rem;
